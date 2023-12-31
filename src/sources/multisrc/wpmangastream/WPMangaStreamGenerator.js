4a0cc6661317cc4dc3e45b623c94137ed9cb4fb1
wilson gabriel <152060461+WilsonMinaya@users.noreply.github.com>
2023-12-31 04:55:06 -0400

    Update WPMangaStreamGenerator.js

--- src/sources/multisrc/wpmangastream/WPMangaStreamGenerator.js
+++ src/sources/multisrc/wpmangastream/WPMangaStreamGenerator.js
@@ -18,3 +18,30 @@ export const LightNovelsBrasilScraper = new WPMangaStreamScraper(
   'https://lightnovelbrasil.com/',
   'Light Novels Brasil',
 );
+import WPMangaStreamScraper from './WPMangaStreamScraper';
+
+export const NobleMtlScraper = new WPMangaStreamScraper(
+  144,
+  'https://noblemtl.com/',
+  'Noble Machine translations',
+  { reverseChapters: true },
+);
+
+export const LiebeSchneeHiverNovelScraper = new WPMangaStreamScraper(
+  123,
+  'https://lshnovel.com/',
+  'Liebe Schnee Hiver Novel',
+);
+
+export const LightNovelsBrasilScraper = new WPMangaStreamScraper(
+  129,
+  'https://lightnovelbrasil.com/',
+  'Light Novels Brasil',
+);
+
+
+export const TuNovelaLigeraScraper = new WPMangaStreamScraper(
+  '171',
+  'https://tunovelaligera.com/',
+  'Tu Novela Ligera',
+);
