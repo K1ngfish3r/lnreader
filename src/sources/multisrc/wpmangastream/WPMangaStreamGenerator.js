038a7908a4a3288c02b9b341c53b6f0611364359
wilson gabriel <152060461+WilsonMinaya@users.noreply.github.com>
2023-12-31 04:45:52 -0400

    Update WPMangaStreamGenerator.js

--- src/sources/multisrc/wpmangastream/WPMangaStreamGenerator.js
+++ src/sources/multisrc/wpmangastream/WPMangaStreamGenerator.js
@@ -18,9 +18,3 @@ export const LightNovelsBrasilScraper = new WPMangaStreamScraper(
   'https://lightnovelbrasil.com/',
   'Light Novels Brasil',
 );
-
-export const LiebeSchneeHiverNovelScraper = new WPMangaStreamScraper(
-  '171',
-  'https://tunovelaligera.com/',
-  'Tu Novela Ligera',
-);
